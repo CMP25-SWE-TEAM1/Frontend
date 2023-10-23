1f90b5ea2013648604f76aa8011932e578e7c9f9
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-10-23 17:25:04 +0300

    Initialize React App & Tailwind

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mt-11 font-bold text-secondary">Test</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+// This is for our custom css configurations (extend tailwind)
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#c0e1eb",
+        tertiary: "#151030",
+        forth: "#67b6d0",
+        fifth: "#00030e",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "475px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/bgs/herobg.jpg')",
+        "phone-pattern": "url('/src/assets/phone_pattern.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
