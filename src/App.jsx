35e60caa4b2d18828c51478e3fec226569133af3
Yousif Mohamed <yousif.infinix@gmail.com>
2023-10-25 15:31:26 +0300

    Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Widgets from "./components/Widgets";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div className="app flex ml-auto mr-auto h-[100vh] pl-10 pr-10">
-      {true && <Sidebar />}
+      
+      {false && <Sidebar />}
       {/* true will be replaced by authorization*/}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="login"></Route>
-          </Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/home" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
-      {true && <Widgets />}
+      {false && <Widgets />}
       {/* true will be replaced by authorization*/}
 
     </div>
