0ccd7f08f5890500ccd12a377e867b7d2b2dd0ea
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-10-26 23:15:56 +0300

    Add dark mode toggling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StyledEngineProvider } from "@mui/material";
 
-
-const htmlElement = document.getElementById("htmlid");
 //For setting dark mode
+// const htmlElement = document.getElementById("htmlid");
 // document.documentElement.style.setProperty("--color-theme", "dark");
 // htmlElement.classList.add("dark");
 
@@ -14,11 +14,12 @@ const htmlElement = document.getElementById("htmlid");
 // document.documentElement.style.setProperty("--color-theme", "white");
 // htmlElement.classList.remove("dark");
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
