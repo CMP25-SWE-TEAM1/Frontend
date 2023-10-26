e98e37d9d4686c68417dd9d6b36b4714383dfecc
Mohamed Taher <muhammed.snd22@gmail.com>
2023-10-27 00:06:00 +0200

    Added Landing page to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Widgets from "./components/Widgets";
 import Home from "./components/Home";
+import Landing from "./components/Landing";
+import { useState } from "react";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="app flex ml-auto mr-auto h-[100vh] pl-10 pr-10">
-      {true && <Sidebar />}
+      {loggedIn && <Sidebar />}
       {/* true will be replaced by authorization*/}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
+          <Route path="/" element={<Landing />}>
             <Route path="login"></Route>
           </Route>
           <Route path="/home" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
-      {true && <Widgets />}
+      {loggedIn && <Widgets />}
       {/* true will be replaced by authorization*/}
-
     </div>
   );
 };
