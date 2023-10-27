767a7fbd07b03ff15f4e20c2de5d9d202b5c931b
Mohamed Taher <muhammed.snd22@gmail.com>
2023-10-27 18:18:20 +0200

    Disabled StrictMode (it causes double useEffect)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const htmlElement = document.getElementById("htmlid");
 //For setting dark mode
@@ -14,12 +13,11 @@ const htmlElement = document.getElementById("htmlid");
 // document.documentElement.style.setProperty("--color-theme", "white");
 // htmlElement.classList.remove("dark");
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
