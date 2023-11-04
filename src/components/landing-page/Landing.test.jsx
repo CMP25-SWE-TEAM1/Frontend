929fb05d44568290c1f7aeaac24d117af5f7b1eb
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-11-04 13:17:12 +0200

    Added landing page unit test (Rendering)

---
+++ src/components/landing-page/Landing.test.jsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react";
+import Landing from "./Landing";
+
+// Mocking methods which are not implemented in JSDOM
+// mocking `matchMedia`
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
+
+describe("Landing page", () => {
+  test("renders correctly", () => {
+    render(<Landing />);
+
+    // Logo image
+    expect(screen.getByRole("img", { name: /logo/i })).toBeInTheDocument();
+
+    // Headers
+    expect(screen.getByText(/happening now/i)).toBeInTheDocument();
+    expect(screen.getByText(/join today\./i)).toBeInTheDocument();
+
+    // Signup Google Button
+    expect(
+      screen.getByRole("button", {
+        name: /sign up with google/i,
+      })
+    ).toBeInTheDocument();
+
+    // Signup Apple Button
+    expect(
+      screen.getByRole("button", {
+        name: /sign up with apple/i,
+      })
+    ).toBeInTheDocument();
+
+    // Create Account
+    expect(
+      screen.getByRole("link", { name: /create account/i })
+    ).toBeInTheDocument();
+
+    // Terms
+    expect(
+      screen.getByText(/by signing up, you agree to the and , including/i)
+    ).toBeInTheDocument();
+
+    // Login
+    expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /sign in/i })).toBeInTheDocument();
+
+    // Footer
+    expect(screen.getByText(/© 2023 gigachat corp\./i)).toBeInTheDocument();
+  });
+});
