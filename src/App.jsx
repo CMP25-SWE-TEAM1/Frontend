350fcb26a6551f2c4991a072573c349b0b04b257
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-11-06 22:56:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Widgets from "./components/Widgets";
-import Home from "./components/Home";
-import SignUp from "./components/SignUp";
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import Widgets from "./components/Widgets"
+import Home from "./components/Home"
+import Login from "./components/Login/Login"
+import PasswordReset from "./components/Login/PasswordReset"
+
+import Landing from "./components/landing-page/Landing"
+import { useState } from "react"
+import { useEffect } from "react"
 
 const App = () => {
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    setLocation(window.location.pathname)
+  }, [location])
+
+  //This will be moved to landing page
+  const [openModal, setOpenModal] = useState(false)
+  const handleOpenModal = () => setOpenModal(true)
+  const handleCloseModal = () => {
+    setOpenModal(false)
+    setLocation(window.location.pathname)
+  }
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return (
-    <div className="app flex ml-auto mr-auto h-[100vh]">
-      {false && <Sidebar />}
+    <div className="app ml-auto mr-auto flex justify-start">
+      {loggedIn && <Sidebar />}
       {/* true will be replaced by authorization*/}
       <BrowserRouter>
-      <Routes>
-          <Route path="/SignUp" element={<SignUp />}></Route>
-          <Route path="/" element={<Home />}></Route>
+        {/* {location !== "/login" && location !== "/password_reset" && <Sidebar />} */}
+        {/* true will be replaced by authorization*/}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Landing openModal={openModal} handleOpenModal={handleOpenModal} handleCloseModal={handleCloseModal} location={location} setLocation={setLocation} />
+              </>
+            }
+          ></Route>
+          <Route path="login" element={<Login openModal={true} handleCloseModal={handleCloseModal} location={location} setLocation={setLocation} />}></Route>
+          <Route path="password_reset" element={<PasswordReset />}></Route>
           <Route path="/home" element={<Home />}></Route>
+          <Route path="*" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
-      {false && <Widgets />}
+      {loggedIn && false && <Widgets />}
       {/* true will be replaced by authorization*/}
-
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
