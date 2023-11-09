3abecdf25e96ea28471dab5dc019440b264adf4f
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-11-09 04:35:14 +0200

    Added nav col (chat - base HTML)

---
+++ src/components/messages-page/navigation/InfoChat.jsx
@@ -0,0 +1,19 @@
+const InfoChat = () => {
+    return ( 
+        <div className="info chat">
+            <div className="search"></div>
+            <div className="contacts">
+                <div className="contact">
+                    <a href="#/username">
+                        <img src="" alt="profile img" />
+                    </a>
+                    <div>
+                        name, @username
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default InfoChat;
