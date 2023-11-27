49a511fd59bba92609c970db65b151ccf6fa8a3a
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-11-27 11:00:40 +0200

    Added message input textarea (initial structure)

--- src/components/messages-page/details/message/MessageInput.jsx
+++ src/components/messages-page/details/message/MessageInput.jsx
@@ -25,8 +25,8 @@ const MessageInput = () => {
             </svg>
           </div>
         </div>
-        <div className="message-text" style={{ fontWeight: "300", fontSize: "15px", padding: "4px 12px", flex: "1", minHeight: "32px", maxHeight: "158px", overflowY: "auto" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <div className="message-text" style={{ fontWeight: "300", fontSize: "15px", padding: "4px 12px", flex: "1" }}>
+          <div placeholder="Start a new message" role="textbox" contentEditable style={{ maxWidth: "391px", minHeight: "24px", maxHeight: "160px", overflowY: "auto" }}></div>
         </div>
         <div className="send-message" style={{ marginLeft: "4px", borderBottomLeftRadius: "9999px", borderTopLeftRadius: "9999px", borderBottomRightRadius: "9999px", borderTopRightRadius: "9999px" }}>
           <div className="send-icon" style={{ width: "36px", height: "36px", display: "flex", justifyContent: "center", alignItems: "center" }}>
