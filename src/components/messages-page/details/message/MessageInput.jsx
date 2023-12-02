a29e21fdb5f79cd3153b06e9993222576e056839
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-02 16:13:00 +0200

    Handled cancel media btn

--- src/components/messages-page/details/message/MessageInput.jsx
+++ src/components/messages-page/details/message/MessageInput.jsx
@@ -66,6 +66,11 @@ const MessageInput = (props) => {
     setSndMsgActv("active")
     console.log(e.target.files[0])
   }
+  const handleMediaCancel = () => {
+    setMediaInput()
+    setMediaInputType()
+    setMediaInputPreview()
+  }
   const getImgURL = () => {}
 
   return (
@@ -119,7 +124,7 @@ const MessageInput = (props) => {
           {mediaInputPreview && (
             <div className="new-message-media-attach">
               <div>
-                <div className="cancel-btn" title="Remove">
+                <div className="cancel-btn" title="Remove" onClick={handleMediaCancel}>
                   <svg viewBox="0 0 24 24" aria-hidden="true">
                     <g>
                       <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
