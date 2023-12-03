54c9e9373cc0bc2e03fedb077c9a623cd8a27fca
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-03 20:13:19 +0200

    Added [file validation, media upload handler]

--- src/components/messages-page/details/MessagesConstants.jsx
+++ src/components/messages-page/details/MessagesConstants.jsx
@@ -1,8 +1,23 @@
 // Socket server
+// ================
 export const SOCKET_ON = false
 export const TENOR_API_KEY = "AIzaSyDE0FD8utv75fMWO_i0DzKDXyLR9-hn77o"
 
 export const SOCKET_IO = {
   mock: "http://localhost:3001",
   actual: "http://gigachat.com??..",
+}
+
+// APIs
+// ================
+const MOCK_DOMAIN = "https://ca224727-23e8-4fb6-b73e-dc8eac260c2d.mock.pstmn.io"
+const ACTUAL_DOMAIN = "http://backend.gigachat.cloudns.org/api"
+
+export const APIs = {
+  mock: {
+    // postMedia: MOCK_DOMAIN + "/media",
+  },
+  actual: {
+    postMedia: ACTUAL_DOMAIN + "/media",
+  },
 }
