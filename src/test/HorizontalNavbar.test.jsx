ddfc6e40eb53129debbf96e3e4d4492165040f9a
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-12-11 14:50:49 +0200

    bug fixes

--- src/test/HorizontalNavbar.test.jsx
+++ src/test/HorizontalNavbar.test.jsx
@@ -1,47 +1,47 @@
-import { act, render, screen } from "@testing-library/react";
-import HorizontalNavbar from "../components/HorizontalNavbar";
-import { BrowserRouter } from "react-router-dom";
-import user from "@testing-library/user-event";
+import { act, render, screen } from "@testing-library/react"
+import HorizontalNavbar from "../components/HorizontalNavbar"
+import { BrowserRouter } from "react-router-dom"
+import user from "@testing-library/user-event"
 
 const navLinks = [
   { title: "For you", location: "foryou" },
   { title: "Following", location: "following" },
-];
-const oUrl = "/home";
+]
+const oUrl = "/home"
 
 describe("Horizontal Navbar", () => {
   test("Renders Props Links", () => {
     render(
       <BrowserRouter>
-        <HorizontalNavbar urls={navLinks} originalUrl={oUrl} />
+        <HorizontalNavbar urls={navLinks} originalUrl={oUrl} handlers={[]} />
       </BrowserRouter>
-    );
+    )
 
     for (const link of navLinks) {
-      const linkElement = screen.getByText(link.title);
-      expect(linkElement).toBeInTheDocument();
+      const linkElement = screen.getByText(link.title)
+      expect(linkElement).toBeInTheDocument()
     }
-  });
+  })
 
   test("Navigating Correctly through Links", async () => {
     render(
       <BrowserRouter>
-        <HorizontalNavbar urls={navLinks} originalUrl={oUrl} />
+        <HorizontalNavbar urls={navLinks} originalUrl={oUrl} handlers={[]} />
       </BrowserRouter>
-    );
+    )
 
     for (const link of navLinks) {
       const linkElement = screen.getByRole("link", {
         name: link.title,
-      });
+      })
 
       await act(async () => {
-        await user.click(linkElement);
-      });
+        await user.click(linkElement)
+      })
 
-      const location = window.location.pathname;
+      const location = window.location.pathname
 
-      expect(location).toBe(oUrl + "/" + link.location);
+      expect(location).toBe(oUrl + "/" + link.location)
     }
-  });
-});
+  })
+})
