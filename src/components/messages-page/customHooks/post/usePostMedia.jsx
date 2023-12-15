d9a4dadc6290edde8553b160d81550c5f6b456de
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-15 13:09:33 +0200

    Added postMedia (mock)

--- src/components/messages-page/customHooks/post/usePostMedia.jsx
+++ src/components/messages-page/customHooks/post/usePostMedia.jsx
@@ -2,17 +2,21 @@ import { APIs, SERVER_ON } from "./../../MessagesConstants"
 import axios from "axios"
 
 // handleUploadMedia: Uploads media to server and get its URL
-const usePostMedia = (mediaFile) => {
+const usePostMedia = (mediaFiles) => {
   const formData = new FormData()
-  formData.append("media", mediaFile)
+  mediaFiles.forEach((file) => {
+    formData.append(`media`, file)
+  })
 
   axios
     .post(SERVER_ON ? APIs.actual.postMedia : APIs.mock.postMedia, formData)
     .then((res) => {
-      console.log(res)
+      console.log(res.data)
+      return res.data.urls
     })
     .catch((err) => {
       console.log(err)
+      return null
     })
 }
 
