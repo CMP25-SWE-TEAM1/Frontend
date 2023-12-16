bc39f33fc10170868308ece7d993cbdaea2a8e79
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-12-16 14:48:40 +0200

    converting custom tabs to be generic

--- src/components/Explore/CustomTabs.jsx
+++ src/components/Explore/CustomTabs.jsx
@@ -11,7 +11,7 @@ function a11yProps(index) {
   }
 }
 
-const CustomTabs = ({ tabValue, handleChangeTabValue }) => {
+const CustomTabs = ({ tabValue, handleChangeTabValue, tabsNames }) => {
   const darkMode = useSelector((state) => state.theme.darkMode)
 
   return (
@@ -33,61 +33,23 @@ const CustomTabs = ({ tabValue, handleChangeTabValue }) => {
           aria-label="basic tabs example"
           variant="scrollable"
         >
-          <Tab
-            label="For you"
-            sx={{
-              width: "20%",
-              fontWeight: tabValue === 0 ? "550" : "100",
-              fontSize: "12px",
-              padding: 0,
-              color: tabValue === 0 && darkMode ? "white" : tabValue === 0 ? "black" : darkMode ? "#9aa1ad" : "",
-            }}
-            {...a11yProps(0)}
-          />
-          <Tab
-            label="Trending"
-            sx={{
-              width: "20%",
-              fontWeight: tabValue === 1 ? "550" : "100",
-              fontSize: "12px",
-              padding: 0,
-              color: tabValue === 1 && darkMode ? "white" : tabValue === 1 ? "black" : darkMode ? "#9aa1ad" : "",
-            }}
-            {...a11yProps(1)}
-          />
-          <Tab
-            label="News"
-            sx={{
-              width: "20%",
-              fontWeight: tabValue === 2 ? "550" : "100",
-              fontSize: "12px",
-              padding: 0,
-              color: tabValue === 2 && darkMode ? "white" : tabValue === 2 ? "black" : darkMode ? "#9aa1ad" : "",
-            }}
-            {...a11yProps(2)}
-          />
-          <Tab
-            label="Sports"
-            sx={{
-              width: "20%",
-              fontWeight: tabValue === 3 ? "550" : "100",
-              fontSize: "12px",
-              padding: 0,
-              color: tabValue === 3 && darkMode ? "white" : tabValue === 3 ? "black" : darkMode ? "#9aa1ad" : "",
-            }}
-            {...a11yProps(3)}
-          />
-          <Tab
-            label="Entertainment"
-            sx={{
-              width: "20%",
-              fontWeight: tabValue === 4 ? "550" : "100",
-              fontSize: "12px",
-              padding: 0,
-              color: tabValue === 4 && darkMode ? "white" : tabValue === 4 ? "black" : darkMode ? "#9aa1ad" : "",
-            }}
-            {...a11yProps(4)}
-          />
+          {
+            tabsNames.map((tab,index) => {
+              return (
+                <Tab
+                  label={tab}
+                  sx={{
+                    width: `${100/tabsNames.length}%`,
+                    fontWeight: tabValue === index ? "550" : "100",
+                    fontSize: "12px",
+                    padding: 0,
+                    color: tabValue === index && darkMode ? "white" : tabValue === index ? "black" : darkMode ? "#9aa1ad" : "",
+                  }}
+                  {...a11yProps(index)}
+                />
+              )
+            })
+          }
         </Tabs>
       </Box>
     </Box>
