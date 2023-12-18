56f4c291c8451ea387fa0d84bb4747b5dfea5a43
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-18 16:38:18 +0200

    Updated contacts time format (minutes, hours, month day) - Updated contact w unread style

--- src/components/messages-page/navigation/Contacts.jsx
+++ src/components/messages-page/navigation/Contacts.jsx
@@ -16,10 +16,36 @@ const Contacts = (props) => {
   const selectedContact = props.selectedContact
   const setSelectedContact = props.setSelectedContact
 
+  const getLastMessageTime = (lastMessageTime) => {
+    const messageDate = new Date(lastMessageTime)
+    const currentDate = new Date()
+
+    const timeDiff = currentDate - messageDate
+    const minutesDiff = Math.floor(timeDiff / (1000 * 60))
+    const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60))
+
+    if (minutesDiff < 60) {
+      return `${minutesDiff} min`
+    } else if (hoursDiff < 24) {
+      const formattedHours = messageDate.toLocaleString("en-US", {
+        hour: "numeric",
+        // minute: "numeric",
+        hour12: true,
+      })
+      return `${formattedHours}`
+    } else {
+      // If the difference is greater than or equal to 24 hours, display the date
+      return messageDate.toLocaleString("en-US", {
+        month: "short",
+        day: "numeric",
+      })
+    }
+  }
+
   return (
     <>
       {contacts.map((contact, index) => (
-        <ListItem disablePadding key={index} sx={selectedContact === contact.id ? { backgroundColor: "#EFF3F4", borderRight: "4px solid #1D9BF0" } : {}}>
+        <ListItem disablePadding key={index} sx={selectedContact === contact.id ? { backgroundColor: "#EFF3F4", borderRight: "4px solid #1D9BF0" } : contact.lastMessageSeen === false ? { backgroundColor: "#F7F9F9" } : {}}>
           <ListItemButton
             onClick={() => {
               console.log("selectedContact", contact.id)
@@ -35,17 +61,7 @@ const Contacts = (props) => {
                 <ListItemAvatar>
                   <Avatar alt={contact.name || "Hamza"} src={contact.avatarLink || "https://64.media.tumblr.com/avatar_f71055191601_128.pnj"} />
                 </ListItemAvatar>
-                <ListItemText
-                  primary={contact.name || "Hamza"}
-                  secondary={`@${contact.userName || "hamza_xyz"} . ${
-                    contact.lastMessageDate
-                      ? new Date(contact.lastMessageDate).toLocaleString("en-US", {
-                          month: "short",
-                          day: "numeric",
-                        })
-                      : ""
-                  }`}
-                />
+                <ListItemText primary={contact.name || "Hamza"} secondary={`@${contact.userName || "hamza_xyz"} . ${contact.lastMessageDate ? getLastMessageTime(contact.lastMessageDate) : ""}`} />
               </div>
               <div style={{ marginTop: "5px" }}>
                 <HighlightedMessage mainText={contact.lastMessage || `${contact.lastMessageMediaType ? (contact.userName === contact.lastMessageSender ? "Sent a" : "You sent a") : ""} ${contact.lastMessageMediaType ? (contact.lastMessageMediaType === "GIF" ? "photo" : "GIF") : ""}`} subText={""} />
