4938d11cec73266ac7dbc34d16c767727c9f13ad
Ismail-Ramadan-Shaheen <114364925+Ismail-Ramadan-Shaheen@users.noreply.github.com>
2023-12-23 09:54:09 +0200

    play and pause video on click bug fixed

--- src/components/Home/DisplayMedia.jsx
+++ src/components/Home/DisplayMedia.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import {useRef, useState} from 'react'
+import { Link } from "react-router-dom"
 
 function DisplayMedia({ mediaUrls, mediaTypes, margin, handleDeleteMedia, showCancelButton}) {
- 
+ const  video = useRef(null);
+ const handleVideoPlay = ()=>{
+  if(video.current.paused)
+  {
+    video.current.play();
+  }else{
+    video.current.pause();
+  }
+ }
   return (
+    <Link>
     <div className={`my-1.5 h-[250px] w-full flex flex-wrap ${mediaUrls.length===0? "hidden": ""}`} >
         {mediaUrls.map((item,index)=>{
           return (mediaTypes[index]=== "jpg"?  (<div key={index} className={`${(mediaUrls.length>1)? "w-[50%]":"w-[100%]"} min-w-[50%] min-h-[50%] grow relative`}>
@@ -24,6 +34,8 @@ function DisplayMedia({ mediaUrls, mediaTypes, margin, handleDeleteMedia, showCa
               alt={`media${index}`}
               loading="lazy"
               controls
+              ref={video}
+              onClick={handleVideoPlay}
           />
           {showCancelButton&&<div className="absolute top-1 right-1 flex backdrop-blur-sm h-8 w-8 items-center justify-center rounded-full dark:bg-[#474b4f] hover:bg-lightHover dark:hover:bg-darkHover hover:cursor-pointer" onClick={()=>handleDeleteMedia(item,index)}>
         x 
@@ -33,6 +45,7 @@ function DisplayMedia({ mediaUrls, mediaTypes, margin, handleDeleteMedia, showCa
           )
         }
         </div>
+        </Link>
   )
 }
 
