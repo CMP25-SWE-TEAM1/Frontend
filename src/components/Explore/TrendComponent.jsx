c2a588971028aa9e2283d13f023d33a60eee01e5
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-12-27 17:18:07 +0200

    notification icons

--- src/components/Explore/TrendComponent.jsx
+++ src/components/Explore/TrendComponent.jsx
@@ -3,6 +3,7 @@ const TrendComponent = ({ index, categoray, name, numberOfPosts, fetchTrendTweet
     <div
       className="flex min-h-[100px] w-full cursor-pointer flex-col justify-between p-4 hover:bg-lightHover dark:hover:bg-darkHover"
       onClick={() => {
+        console.log("HH")
         window.location.href = `/search?q=${name.replace(/#/g, "%23")}`
       }}
     >
--- src/components/Notifications/NotificationComponent.jsx
+++ src/components/Notifications/NotificationComponent.jsx
@@ -4,6 +4,9 @@ import { Avatar } from "@mui/material"
 import PersonAddIcon from "@mui/icons-material/PersonAdd"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import CachedIcon from "@mui/icons-material/Cached"
+import MessageIcon from "@mui/icons-material/Message"
+import ReplyIcon from "@mui/icons-material/Reply"
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail"
 
 import { APIs } from "../../constants/signupConstants"
 import { useEffect, useState } from "react"
@@ -24,17 +27,22 @@ const NotificationComponent = ({ logo, type, text, date, notifier }) => {
 
   const userToken = useSelector((state) => state.user.token)
 
-
-
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   return (
-    <div onClick={() => {
-      navigate(`/${notifier ? notifier.username : text.split(" ")[0]}`)
-    }} className="flex min-h-[64px] cursor-pointer flex-col items-center pb-3 pl-4 pr-4 pt-3 hover:bg-lightHover dark:hover:bg-darkHover">
+    <div
+      onClick={() => {
+        navigate(`/${notifier ? notifier.username : text.split(" ")[0]}`)
+      }}
+      className="flex min-h-[64px] cursor-pointer flex-col items-center pb-3 pl-4 pr-4 pt-3 hover:bg-lightHover dark:hover:bg-darkHover"
+    >
       <div className="flex items-center gap-3 self-start">
         {type === "retweet" && <CachedIcon className="text-4xl text-green-500" />}
         {type === "like" && <FavoriteIcon className="text-4xl text-red-500" />}
         {type === "follow" && <PersonAddIcon className="text-4xl text-blue-500" />}
+        {type === "mention" && <AlternateEmailIcon className="text-4xl text-green-500" />}
+        {type === "message" && <MessageIcon className="text-4xl text-blue-500" />}
+        {type === "reply" && <ReplyIcon className="text-4xl text-blue-500" />}
+
         <Avatar className="mr-3" alt={"j"} src={logo} sx={{ width: 30, height: 30 }} />
       </div>
       <div className="text ml-14 mt-2 flex-1 self-start ">
