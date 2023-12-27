dc7822b2c6a30ca3143ee6d0ecb3ab66acf9f1ef
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-27 21:51:43 +0200

    Added MessageTool test (render, copy to clipboard)

---
+++ src/components/messages-page/details/message/MessageTools.test.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { render, screen, fireEvent, act } from "@testing-library/react"
+import MessageTools from "./MessageTools"
+import copyToClipboard from "copy-to-clipboard"
+
+// Mock the copy-to-clipboard library
+jest.mock("copy-to-clipboard")
+
+describe.only("MessageTools testing", () => {
+  test("renders successfully", () => {
+    // Mock props
+    const props = {
+      messageId: "123",
+      messageMedia: "www.website.com/mediaLink.jpg",
+      messageText: "Just find me",
+      hideMsgTools: jest.fn(),
+      msgToolsPositionX: "L",
+      msgToolsPositionY: "T",
+      visibiltyStyle: "block",
+    }
+
+    // Render the component
+    render(<MessageTools {...props} />)
+
+    // Assert that the message tools is initially visible
+    expect(screen.getByTestId("message-tools")).toHaveStyle({ display: "block" })
+  })
+
+  test("copy-to-clipboard successfully", () => {
+    // Mock props
+    const props = {
+      messageId: "123",
+      messageMedia: "www.website.com/mediaLink.jpg",
+      messageText: "Just find me",
+      hideMsgTools: jest.fn(),
+      msgToolsPositionX: "L",
+      msgToolsPositionY: "T",
+      visibiltyStyle: "block",
+    }
+
+    // Render the component
+    render(<MessageTools {...props} />)
+
+    // Simulate a click on the copy tool
+    fireEvent.click(screen.getByText("Copy message"))
+
+    // Assert that the hideMsgTools function is called
+    expect(props.hideMsgTools).toHaveBeenCalled()
+
+    // Assert that copyToClipboard is called with the expected text
+    expect(copyToClipboard).toHaveBeenCalledWith("Just find me\nwww.website.com/mediaLink.jpg")
+
+    // wait for the alert to appear
+    act(() => jest.advanceTimersByTime(1))
+
+    // Assert that "Copied to clipboard" alert is visible
+    expect(screen.getByText("Copied to clipboard")).toBeInTheDocument()
+  })
+})
