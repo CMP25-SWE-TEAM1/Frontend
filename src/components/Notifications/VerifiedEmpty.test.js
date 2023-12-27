c4358e14d96796bf0724a5937ef4a469f9bb64cc
Mohamed Taher <mohamed.taher0x3@gmail.com>
2023-12-27 23:03:08 +0200

    Added VerifiedEmpty testing (render)

---
+++ src/components/Notifications/VerifiedEmpty.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import VerifiedEmpty from "./VerifiedEmpty"
+
+describe.only("VerifiedEmpty", () => {
+  test("renders correctly", () => {
+    render(<VerifiedEmpty />)
+
+    // Assert that the image is rendered
+    expect(screen.getByAltText("nothing")).toBeInTheDocument()
+
+    // Assert that the NotificationEmpty component is rendered
+    expect(screen.getByText("Nothing to see here — yet")).toBeInTheDocument()
+  })
+})
