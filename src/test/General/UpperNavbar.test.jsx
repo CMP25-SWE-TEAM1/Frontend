d0f858125b8902f7ba70e58c3d58cd0e0523e82a
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-12-28 13:21:45 +0200

    .

---
+++ src/test/General/UpperNavbar.test.jsx
@@ -0,0 +1,17 @@
+import { screen, render } from "@testing-library/react";
+// import UpperNavfbar from "../../../components/General/UpperNavbar";
+import { BrowserRouter } from "react-router-dom"
+
+describe("testing UpperNavbar component",()=>{
+    test("rendering UpperNavbar correctly",()=>{
+        // render(
+        // <BrowserRouter>
+        // <UpperNavbar name="upper"/>
+        // </BrowserRouter>
+        // );
+        // const navbarName = screen.getByText("upper");
+        // expect(navbarName).toBeInTheDocument();
+        // const backArrow = screen.getByTestId("backArrow");
+        // expect(backArrow).toBeInTheDocument();
+    });
+});
