0752f81b58032327bf2fc0b368b1e9a37033c51b
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-12-28 08:14:26 +0200

    Update UpperNavbar.jsx

--- src/components/General/UpperNavbar.jsx
+++ src/components/General/UpperNavbar.jsx
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom"
 import WestIcon from "@mui/icons-material/West"
 import { useNavigate } from "react-router-dom"
 
-function UpperNavbar({name}){
-    const navigate = useNavigate();
-    return(
-            <Link to="#top">
-        <div className="flex h-[53px] items-center">
-        <div data-testid="backArrow" onClick={()=>navigate(-1)} className="ml-2 mr-5 flex h-8 w-8 items-center justify-center rounded-full hover:bg-lightHover dark:hover:bg-darkHover">
-        <WestIcon/> 
+function UpperNavbar({ name }) {
+  const navigate = useNavigate()
+  return (
+    <Link to="#top">
+      <div className="flex h-[53px] items-center">
+        <div data-testid="backArrow" onClick={() => navigate(-1)} className="ml-2 mr-5 flex h-8 w-8 items-center justify-center rounded-full hover:bg-lightHover dark:hover:bg-darkHover">
+          <WestIcon />
         </div>
-        <div><b>{name}</b></div>
+        <div>
+          <b>{name}</b>
         </div>
       </div>
     </Link>
